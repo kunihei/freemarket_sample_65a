ef131981247bdd6a2a3051b89b5c62cfcc1d9ffb
kousaka0727 <kousaka.j@icloud.com>
2019-12-26 20:52:19 +0900

    ignore

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :items, only: [:show, :edit]
 
   resources :users, only: [:index, :show, ]
+
+  get 'users/new/:name', controller: 'users', action: 'new'
   get 'users/edit/:name', controller: 'users', action: 'edit'
 end
