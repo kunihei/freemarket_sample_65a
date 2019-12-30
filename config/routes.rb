7784dc0576d82cab90acc60bc0c43c5544e9480f
kousaka0727 <kousaka.j@icloud.com>
2019-12-30 12:16:01 +0900

    scss調節

--- config/routes.rb
+++ config/routes.rb
@@ -17,11 +17,8 @@ Rails.application.routes.draw do
 
   
 
-  #resources :users, only: [:index, :show]
-  #get 'users/new/:name', controller: 'users', action: 'create'
-
-
-
+  resources :users, only: [:index, :show]
+  get 'users/new/:name', controller: 'users', action: 'create'
   get 'users/edit/:name', controller: 'users', action: 'edit'
 
 end
