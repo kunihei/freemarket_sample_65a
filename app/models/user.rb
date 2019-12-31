6689540ab7183740bb0d1ea4e4c95bf6cae61fe3
kousaka0727 <kousaka.j@icloud.com>
2019-12-31 23:06:33 +0900

    phone_number　アソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :address
+  has_one :phone_number
 end
