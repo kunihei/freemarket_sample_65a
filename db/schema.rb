f7e7883d0f12f666a0fe5fa8a4910c92c8fe71db
kousaka0727 <kousaka.j@icloud.com>
2019-12-31 23:11:28 +0900

    cardテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_30_101443) do
+ActiveRecord::Schema.define(version: 2019_12_31_141034) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postcode", null: false
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2019_12_30_101443) do
     t.string "first_name_kana"
     t.index ["tell"], name: "index_addresses_on_tell", unique: true
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
+    t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -73,5 +80,6 @@ ActiveRecord::Schema.define(version: 2019_12_30_101443) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cards", "users"
   add_foreign_key "phone_numbers", "users"
 end
