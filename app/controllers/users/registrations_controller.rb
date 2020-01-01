ccfd0bf69f630e142ce0e088daa591e32ad5a133
kousaka0727 <kousaka.j@icloud.com>
2020-01-01 23:24:18 +0900

    cards view 変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -47,6 +47,22 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   
   def create_cards
+    Payjp.api_key = "sk_test_f67be4ad1051de6822903d38"
+    if params['payjp-token'].blank?
+      render 'mypages/create_card'
+    else
+      customer = Payjp::Customer.create( # ここで先ほど生成したトークンを顧客情報と紐付け、PAY.JP管理サイトに送信
+        email: current_user.email,
+        card: params['payjp-token'],
+        metadata: {user_id: current_user.id} # 記述しなくても大丈夫です
+      )
+      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if @card.save
+        redirect_to cards_path
+      else
+        render 'mypages/create_card'
+      end
+    end
     render :new_finish
   end
 
