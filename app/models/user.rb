0eb262f61b3c35c84ffee6dcd1379377cb94142c
kousaka0727 <kousaka.j@icloud.com>
2020-01-04 14:32:31 +0900

    バリデーション(user)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@ class User < ApplicationRecord
   has_one  :address
   has_one  :number
   has_many :cards
+
+
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A\S+@\S+\.\S+\z/ }
+  validates :password, presence: true, length: { minimum: 7 }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
 end
