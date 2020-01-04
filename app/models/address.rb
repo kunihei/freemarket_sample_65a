7498cf2d9db351c78367eb86a3ed26e26be1f388
kousaka0727 <kousaka.j@icloud.com>
2020-01-04 14:33:05 +0900

    バリデーション(address)

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,4 +3,14 @@ class Address < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to :user, optional: true
   belongs_to :item, optional: true
+
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :postcode, presence: true, format: { with: /\A\d{3}[-]\d{4}\z/ }
+  validates :city, presence: true
+  validates :block, presence: true
+  validates :building
+  validates :tell, uniqueness: true, format: { with:  /\A\d{11}\z/ }
 end
