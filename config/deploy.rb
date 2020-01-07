f1da9dae85840a66dfb8afd7f25fef3b0f3e67f1
h-asuka <h_langley_a@yahoo.co.jp>
2020-01-07 20:46:04 +0900

    s3 修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,15 +25,6 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    # invoke 'unicorn:restart'
-    invoke 'unicorn:stop'
-    invoke 'unicorn:start'
-  end
-end
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
@@ -42,7 +33,7 @@ set :default_env, {
 }
 
 # secrets.yml用のシンボリックリンクを追加
-set :linked_files, %w{ config/credentials.yml }
+set :linked_files, %w{ config/master.key }
 
 # 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
 
@@ -52,15 +43,17 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
-  desc 'upload credentials.yml'
+  desc 'upload master.key'
   task :upload do
     on roles(:app) do |host|
       if test "[ ! -d #{shared_path}/config ]"
         execute "mkdir -p #{shared_path}/config"
       end
-      upload!('config/credentials.yml', "#{shared_path}/config/credentials.yml")
+      upload!('config/master.key', "#{shared_path}/config/master.key")
     end
   end
   before :starting, 'deploy:upload'
   after :finishing, 'deploy:cleanup'
 end
+
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
