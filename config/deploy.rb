77d6357ba84ca0e0cc03e275a5fa3a06a30f5cc3
h-asuka <h_langley_a@yahoo.co.jp>
2020-01-07 18:03:58 +0900

    s3

--- config/deploy.rb
+++ config/deploy.rb
@@ -52,7 +52,7 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
-  desc 'upload secrets.yml'
+  desc 'upload credentials.yml'
   task :upload do
     on roles(:app) do |host|
       if test "[ ! -d #{shared_path}/config ]"
