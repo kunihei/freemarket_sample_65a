ee31eb695eb8cafe519972585d1151d1d544cfbc
kunihei <kunihei@users.noreply.github.com>
2020-01-14 18:44:55 +0900

    軽微な修正

--- app/assets/javascripts/link_to.js
+++ app/assets/javascripts/link_to.js
@@ -1,6 +1,6 @@
 $(window).on("turbolinks:load",function() {
-  $('.link2').on('submit', function(e){
-    // 通常のsubmitイベントを止める
+  $('.link2').on('linl_to', function(e){
+    // 指定のlink_toのイベントを止める
     e.preventDefault();
   });
 });
