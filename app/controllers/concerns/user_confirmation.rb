2ef3c4291728a68f3217ea177cc36a81e6800691
kousaka0727 <kousaka.j@icloud.com>
2020-01-15 18:39:07 +0900

    エラーハンドリング

--- app/controllers/concerns/user_confirmation.rb
+++ app/controllers/concerns/user_confirmation.rb
@@ -3,8 +3,8 @@ module UserConfirmation
 
   private
   def user_confirmation
-    if @user.id == current_user.id
-      lash[:alert] = "該当ユーザーではありません"
+    if @user.id != current_user.id
+      flash[:alert] = "該当ユーザーではありません"
       redirect_to root_path
     end
   end
